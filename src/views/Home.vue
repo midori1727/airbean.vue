d450f37e2cb04865d6a0ec40ef060e00ed1c8232
rojinyilmaz <70367334+rojinyilmaz@users.noreply.github.com>
2021-02-16 19:45:33 +0100

    Updated Home.vue
    Added design on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<div class="wrapper">
+<main class="main">
+    <img alt="logo" src="../assets/airbean_logo.svg"/>
+</main>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+  
   }
 };
 </script>
+
+<style scoped>
+
+.wrapper {
+    height: 89vh;
+    margin-left: -1%;
+  margin-right: -1%;
+  margin-bottom: -1%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #38846d;
+    text-align: center;
+    background-image: url("../assets/header_right.svg"), url("../assets/header_left.svg");
+    background-position: right, left;
+    background-repeat: no-repeat, no-repeat;
+} 
+
+
+
+
+</style>
